3aa90b565c36830cf7d3fc6cf0776faa3dc2bfff
dusty-turner <dusty.s.turner.mil@mail.mil>
2020-10-18 22:14:29 -0400

    updates to collaboration chapter

--- render_script.R
+++ render_script.R
@@ -1,5 +1,6 @@
 library(bookdown)
-bookdown::render_book("Intermediate_R.Rmd", "bookdown::gitbook",output_dir = "MORS_ETF_2020",clean_envir = TRUE)
+# bookdown::render_book("Intermediate_R.Rmd", "bookdown::gitbook",output_dir = "MORS_ETF_2020",clean_envir = TRUE)
+bookdown::render_book("ETF-R-Git.Rmd", "bookdown::gitbook",output_dir = "MORS_ETF_2020",clean_envir = TRUE)
 # serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 # rmarkdown::render_site(encoding = 'UTF-8') #HTML and PDF
 
